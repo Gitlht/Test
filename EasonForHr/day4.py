a6832f2c0a031811e221dd0cb4069cc0987b90de
Gitlht <qq535849868@163.com>
2022-07-26 22:45:59 +0800

    day4

---
+++ EasonForHr/day4.py
@@ -0,0 +1,14 @@
+class car:
+    wheel_sum = 4
+
+    @staticmethod
+    def space(l, w, h):
+        v = l * w * h
+        return v
+
+    def __init__(self, brand):
+        self.brand = brand
+
+
+car1 = car("宝马")
+print(car1.brand, car1.space(2, 3, 5),car1.wheel_sum)
